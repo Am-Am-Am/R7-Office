d7e785f56e39baa9321f4e8b05fc883b9256ae09
Amur Almukhametov <55045450+Am-Am-Am@users.noreply.github.com>
2023-05-23 13:18:55 +0500

    Очистка значений

--- setDefaultValues.js
+++ setDefaultValues.js
@@ -1,7 +1,14 @@
-// Функция для удаления границ ячейки - перекрашиваем в белый
+// Очистка значений
 let area = secondWorksheet.GetRange("A21:F150"); // Сброс предыдущих значений
 
+function aroundWhiteBorder(el) {
+    el.SetBorders("Bottom", "Medium", Api.CreateColorFromRGB(255, 255, 255));
+    el.SetBorders("Top", "Medium", Api.CreateColorFromRGB(255, 255, 255));
+    el.SetBorders("Left", "Medium", Api.CreateColorFromRGB(255, 255, 255));
+    el.SetBorders("Right", "Medium", Api.CreateColorFromRGB(255, 255, 255));
+}
+
 area.ForEach((x) => {
-x.SetValue("");
-aroundWhiteBorder(x);
+    x.SetValue("");
+    aroundWhiteBorder(x);
 });
