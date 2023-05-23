a97fc4362316c9af79edfdfb61dff04c462cdb1e
Amur Almukhametov <55045450+Am-Am-Am@users.noreply.github.com>
2023-05-23 13:07:57 +0500

    Получение ячеек из диапазона

--- getCellFromRange.js
+++ getCellFromRange.js
@@ -1,3 +1,3 @@
-var oWorksheet = Api.GetActiveSheet();
-var oRange = oWorksheet.GetRange("A1:C3");
-oRange.GetCells(2, 1).SetFillColor(Api.CreateColorFromRGB(255, 224, 204));
+let oWorksheet = Api.GetActiveSheet(); //Получение текущего листа
+let oRange = oWorksheet.GetRange("A1:C3"); //Получение диапазона ячеек
+oRange.GetCells(2, 1).SetFillColor(Api.CreateColorFromRGB(255, 224, 204)); //Получение ячеек из диапазона
